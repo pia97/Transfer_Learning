1a3498678aed32ea49c659425362318f70d34ea1
pia97 <PiaLehmann@kabelmail.de>
2020-08-06 17:50:25 +0200

    finished experiment modification

--- transfer_learning.py
+++ transfer_learning.py
@@ -9,12 +9,16 @@ import sys
 from tensorflow import keras
 import tensorflow as tf
 
+memory_limit = 10240
+
 # initialization should not allocate all memory on the device
+# also limit memory to memory_limit
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
+            tf.config.experimental.set_virtual_device_configuration( gpu, [ tf.config.experimental.VirtualDeviceConfiguration( memory_limit = memory_limit ) ] )
             break
     except RuntimeError as e:
         print(e)
@@ -53,11 +57,11 @@ models = ['CNN', 'RESNET', 'ENCODER', 'INCEPTION']
 #frozen_layers_Inception = [0, 10, 19, 32, 41, 50, 63]
 
 
-epochs = [1]
-frozen_layers_Cnn = [11]
-frozen_layers_Resnet = [37]
-frozen_layers_Encoder = [22]
-frozen_layers_Inception = [63]
+epochs = [100, 1000]
+frozen_layers_Cnn = [0, 11]
+frozen_layers_Resnet = [0, 37]
+frozen_layers_Encoder = [0, 22]
+frozen_layers_Inception = [0, 63]
 
 gpu = 1
 
@@ -67,12 +71,11 @@ with open(file_path, 'w') as fd:
     fd.write('model;pre_trained_set;training_set;num_frozen_layers;epochs;accuracy\n')
     fd.close()
 
-"""
+#b = True
 with tf.device('/device:GPU:1'):
+#if b == True:
     for dataset_name in UCR86:
-        # for dataset_name in dataset_names:
 
-        # Ouput directory for each dataset
         output_dataset = output_directory+'/'+dataset_name+'/'
 
         for m in models:
@@ -86,62 +89,9 @@ with tf.device('/device:GPU:1'):
 
         data_instance = Data(input_directory, dataset_name, reshape=True)
 
-        for transfer_set in transfer_names:
-            # for transfer_set in dataset_names:
+        transfer_names = transfered[ dataset_name ]
 
-            if transfer_set != dataset_name:
-
-                for eps in epochs:
-
-                    for num_layers in frozen_layers_Cnn:
-
-                        cnn = Cnn(data_instance, output_dataset + '/CNN')
-                        cnn.build_model()
-                        cnn.retrain(pre_trained_directory, eps, 16 *
-                                    gpu, transfer_set, file_path, num_layers)
-
-                    for num_layers in frozen_layers_Resnet:
-
-                        res = Resnet(data_instance, output_dataset + '/RESNET')
-                        res.build_model()
-                        res.retrain(pre_trained_directory, eps, 16 * gpu,
-                                    transfer_set, file_path, num_layers=num_layers)
-
-                    for num_layers in frozen_layers_Encoder:
-
-                        enc = Encoder(data_instance, output_dataset + '/ENCODER')
-                        enc.build_model()
-                        enc.retrain(pre_trained_directory, eps, 12,
-                                    transfer_set, file_path, num_layers=num_layers)
-
-                    for num_layers in frozen_layers_Inception:
-
-                        inc = Inception(
-                            data_instance, output_dataset + '/INCEPTION')
-                        inc.build_model()
-                        inc.retrain(pre_trained_directory, eps, 64 * gpu,
-                                    transfer_set, file_path, num_layers=num_layers)
-"""
-for dataset_name in UCR86:
-    # for dataset_name in dataset_names:
-
-    # Ouput directory for each dataset
-    output_dataset = output_directory+'/'+dataset_name+'/'
-
-    for m in models:
-        path = output_dataset + '/' + m
-
-        if not os.path.exists(path):
-            os.makedirs(path)
-            print("Directory ", path,  " Created ")
-        else:
-            print("Directory ", path,  " already exists")
-
-    data_instance = Data(input_directory, dataset_name, reshape=True)
-
-    transfer_names = transfered[ dataset_name ]
-    for transfer_set in transfer_names:
-        # for transfer_set in dataset_names:
+        transfer_set = transfer_names[0]
 
         if transfer_set != dataset_name:
 
@@ -175,3 +125,139 @@ for dataset_name in UCR86:
                     inc.build_model()
                     inc.retrain(pre_trained_directory, eps, 64 * gpu,
                                 transfer_set, file_path, num_layers=num_layers)
+
+    for dataset_name in UCR86:
+
+        data_instance = Data(input_directory, dataset_name, reshape=True)
+
+        transfer_names = transfered[ dataset_name ]
+
+        transfer_set = transfer_names[1]
+
+        if transfer_set != dataset_name:
+
+            for eps in epochs:
+
+                for num_layers in frozen_layers_Cnn:
+
+                    cnn = Cnn(data_instance, output_dataset + '/CNN')
+                    cnn.build_model()
+                    cnn.retrain(pre_trained_directory, eps, 16 *
+                                gpu, transfer_set, file_path, num_layers)
+
+                for num_layers in frozen_layers_Resnet:
+
+                    res = Resnet(data_instance, output_dataset + '/RESNET')
+                    res.build_model()
+                    res.retrain(pre_trained_directory, eps, 16 * gpu,
+                                transfer_set, file_path, num_layers=num_layers)
+
+                for num_layers in frozen_layers_Encoder:
+
+                    enc = Encoder(data_instance, output_dataset + '/ENCODER')
+                    enc.build_model()
+                    enc.retrain(pre_trained_directory, eps, 12,
+                                    transfer_set, file_path, num_layers=num_layers)
+
+                for num_layers in frozen_layers_Inception:
+
+                    inc = Inception(
+                        data_instance, output_dataset + '/INCEPTION')
+                    inc.build_model()
+                    inc.retrain(pre_trained_directory, eps, 64 * gpu,
+                                transfer_set, file_path, num_layers=num_layers)
+
+    for dataset_name in UCR86:
+
+        data_instance = Data(input_directory, dataset_name, reshape=True)
+
+        transfer_names = transfered[ dataset_name ]
+
+        transfer_set = transfer_names[2]
+
+        if transfer_set != dataset_name:
+
+            for eps in epochs:
+
+                for num_layers in frozen_layers_Cnn:
+
+                    cnn = Cnn(data_instance, output_dataset + '/CNN')
+                    cnn.build_model()
+                    cnn.retrain(pre_trained_directory, eps, 16 *
+                                gpu, transfer_set, file_path, num_layers)
+
+                for num_layers in frozen_layers_Resnet:
+
+                    res = Resnet(data_instance, output_dataset + '/RESNET')
+                    res.build_model()
+                    res.retrain(pre_trained_directory, eps, 16 * gpu,
+                                transfer_set, file_path, num_layers=num_layers)
+
+                for num_layers in frozen_layers_Encoder:
+
+                    enc = Encoder(data_instance, output_dataset + '/ENCODER')
+                    enc.build_model()
+                    enc.retrain(pre_trained_directory, eps, 12,
+                                    transfer_set, file_path, num_layers=num_layers)
+
+                for num_layers in frozen_layers_Inception:
+
+                    inc = Inception(
+                        data_instance, output_dataset + '/INCEPTION')
+                    inc.build_model()
+                    inc.retrain(pre_trained_directory, eps, 64 * gpu,
+                                transfer_set, file_path, num_layers=num_layers)
+"""
+for dataset_name in UCR86:
+    # for dataset_name in dataset_names:
+
+    # Ouput directory for each dataset
+    output_dataset = output_directory+'/'+dataset_name+'/'
+
+    for m in models:
+        path = output_dataset + '/' + m
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+            print("Directory ", path,  " Created ")
+        else:
+            print("Directory ", path,  " already exists")
+
+    data_instance = Data(input_directory, dataset_name, reshape=True)
+
+    transfer_names = transfered[ dataset_name ]
+    transfer_set = transfer_names[0]
+    if transfer_set != dataset_name:
+
+        for eps in epochs:
+
+            for num_layers in frozen_layers_Cnn:
+
+                cnn = Cnn(data_instance, output_dataset + '/CNN')
+                cnn.build_model()
+                cnn.retrain(pre_trained_directory, eps, 16 *
+                            gpu, transfer_set, file_path, num_layers)
+
+            for num_layers in frozen_layers_Resnet:
+
+                res = Resnet(data_instance, output_dataset + '/RESNET')
+                res.build_model()
+                res.retrain(pre_trained_directory, eps, 16 * gpu,
+                            transfer_set, file_path, num_layers=num_layers)
+
+            for num_layers in frozen_layers_Encoder:
+
+                enc = Encoder(data_instance, output_dataset + '/ENCODER')
+                enc.build_model()
+                enc.retrain(pre_trained_directory, eps, 12,
+                                transfer_set, file_path, num_layers=num_layers)
+
+            for num_layers in frozen_layers_Inception:
+
+                inc = Inception(
+                    data_instance, output_dataset + '/INCEPTION')
+                inc.build_model()
+                inc.retrain(pre_trained_directory, eps, 64 * gpu,
+                            transfer_set, file_path, num_layers=num_layers)
+
+"""
