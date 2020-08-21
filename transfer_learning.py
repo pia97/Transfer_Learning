c745c878addb1f22b7a728e14c0b7f3c37114c9f
OMRAN <omran-amayreh@hotmail.com>
2020-08-21 08:55:59 +0200

    One GPU

--- transfer_learning.py
+++ transfer_learning.py
@@ -11,30 +11,32 @@ import tensorflow as tf
 
 memory_limit = 10240
 
+os.environ["CUDA_VISIBLE_DEVICES"]="1" 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
 # initialization should not allocate all memory on the device
 # also limit memory to memory_limit
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-            tf.config.experimental.set_virtual_device_configuration( gpu, [ tf.config.experimental.VirtualDeviceConfiguration( memory_limit = memory_limit ) ] )
-            break
-    except RuntimeError as e:
-        print(e)
+#gpus = tf.config.experimental.list_physical_devices('GPU')
+#if gpus:
+#    try:
+#        for gpu in gpus:
+#            tf.config.experimental.set_memory_growth(gpu, True)
+#            tf.config.experimental.set_virtual_device_configuration( gpu, [ tf.config.experimental.VirtualDeviceConfiguration( memory_limit = memory_limit ) ] )
+#            break
+#    except RuntimeError as e:
+#        print(e)
 
 # Input data directory, for UCR archive 128
-#input_directory = '/home/alamayreh/UCRArchive_2018'
-input_directory = '/home/pia/Dokumente/Studium/AI/UCRArchive_2018'
+input_directory = '/home/alamayreh/pia/UCRArchive_2018'
+#input_directory = '/home/pia/Dokumente/Studium/AI/UCRArchive_2018'
 
 
 # Results directory
-#output_directory = '/home/alamayreh/pia_resutls/results_transfer'
-output_directory = '/home/pia/Dokumente/Studium/AI/Transfer_Learning/results'
+output_directory = '/home/alamayreh/pia/results_transfer'
+#output_directory = '/home/pia/Dokumente/Studium/AI/Transfer_Learning/results'
 
 # Input data directory, for pretrained models
-#pre_trained_directory = '/home/alamayreh/results_training_backup'
-pre_trained_directory = '/home/pia/Dokumente/Studium/AI/tl-results'
+pre_trained_directory = '/home/alamayreh/pia/results_training_backup'
+#pre_trained_directory = '/home/pia/Dokumente/Studium/AI/tl-results'
 
 # List of the datasets
 #dataset_names = pd.read_csv('DataSummary.csv')
@@ -72,59 +74,59 @@ file_path = output_directory + '/results.csv'
 #    fd.close()
 
 #b = True
-with tf.device('/device:GPU:1'):
+#with tf.device('/device:GPU:1'):
 #if b == True:
-    for dataset_name in UCR86:
+for dataset_name in UCR86:
 
-        output_dataset = output_directory+'/'+dataset_name+'/'
+    output_dataset = output_directory+'/'+dataset_name+'/'
 
-        for m in models:
-            path = output_dataset + '/' + m
+    for m in models:
+        path = output_dataset + '/' + m
 
-            if not os.path.exists(path):
-                os.makedirs(path)
-                print("Directory ", path,  " Created ")
-            else:
-                print("Directory ", path,  " already exists")
+        if not os.path.exists(path):
+            os.makedirs(path)
+            print("Directory ", path,  " Created ")
+        else:
+            print("Directory ", path,  " already exists")
 
-        data_instance = Data(input_directory, dataset_name, reshape=True)
+    data_instance = Data(input_directory, dataset_name, reshape=True)
 
-        transfer_names = transfered[ dataset_name ]
+    transfer_names = transfered[ dataset_name ]
 
-        transfer_set = transfer_names[0]
+    transfer_set = transfer_names[0]
 
-        if transfer_set != dataset_name:
+    if transfer_set != dataset_name:
 
-            for eps in epochs:
+        for eps in epochs:
 
-                for num_layers in frozen_layers_Cnn:
+            for num_layers in frozen_layers_Cnn:
 
-                    cnn = Cnn(data_instance, output_dataset + '/CNN')
-                    cnn.build_model()
-                    cnn.retrain(pre_trained_directory, eps, 16 *
-                                gpu, transfer_set, file_path, num_layers)
+                cnn = Cnn(data_instance, output_dataset + '/CNN')
+                cnn.build_model()
+                cnn.retrain(pre_trained_directory, eps, 16 *
+                            gpu, transfer_set, file_path, num_layers)
 
-                for num_layers in frozen_layers_Resnet:
+            for num_layers in frozen_layers_Resnet:
 
-                    res = Resnet(data_instance, output_dataset + '/RESNET')
-                    res.build_model()
-                    res.retrain(pre_trained_directory, eps, 16 * gpu,
+                res = Resnet(data_instance, output_dataset + '/RESNET')
+                res.build_model()
+                res.retrain(pre_trained_directory, eps, 16 * gpu,
+                            transfer_set, file_path, num_layers=num_layers)
+
+            for num_layers in frozen_layers_Encoder:
+
+                enc = Encoder(data_instance, output_dataset + '/ENCODER')
+                enc.build_model()
+                enc.retrain(pre_trained_directory, eps, 12,
                                 transfer_set, file_path, num_layers=num_layers)
 
-                for num_layers in frozen_layers_Encoder:
+            for num_layers in frozen_layers_Inception:
 
-                    enc = Encoder(data_instance, output_dataset + '/ENCODER')
-                    enc.build_model()
-                    enc.retrain(pre_trained_directory, eps, 12,
-                                    transfer_set, file_path, num_layers=num_layers)
-
-                for num_layers in frozen_layers_Inception:
-
-                    inc = Inception(
-                        data_instance, output_dataset + '/INCEPTION')
-                    inc.build_model()
-                    inc.retrain(pre_trained_directory, eps, 64 * gpu,
-                                transfer_set, file_path, num_layers=num_layers)
+                inc = Inception(
+                    data_instance, output_dataset + '/INCEPTION')
+                inc.build_model()
+                inc.retrain(pre_trained_directory, eps, 64 * gpu,
+                            transfer_set, file_path, num_layers=num_layers)
 
 """
     for dataset_name in UCR86:
