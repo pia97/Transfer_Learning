6451ca3ab1bd71c3fe0bfe1edf0d7e5ccd71be96
OMRAN <omran-amayreh@hotmail.com>
2020-08-21 08:58:41 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 start_time=`date +%s`
 python transfer_learning.py
 end_time=`date +%s`
-echo execution time was `expr $end_time - $start_time` s. > /home/pia/Dokumente/Studium/AI/Transfer Learning Server/time.log
+echo execution time was `expr $end_time - $start_time` s. > /home/alamayreh/pia/time.log
